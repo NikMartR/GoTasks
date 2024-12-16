08cb0111769fa625eb2d5fda2dcefd0b8578813d
NikMartR <nrmartianova@gmail.com>
2024-12-16 20:05:11 +0300

    Update client.go

--- task2/client.go
+++ task2/client.go
@@ -2,83 +2,42 @@ package main
 
 import (
   "fmt"
-  "bytes"
-  "context"
-  "encoding/json"
-  "io"
-  "net/http"
-  "time"
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func main() {
-  client := &http.Client{}
-  ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-  defer cancel()
+	client := &http.Client{}
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
 
-  baseURL := "http://localhost:8080"
+	fmt.Println("Requesting /version")
+	versionResp, err := client.Get("http://localhost:8080/version")
+	handleResponse("GET /version", versionResp, err)
 
-  versionResp, err := doGet(ctx, client, baseURL+"/version")
-  if err != nil {
-  fmt.Println("Ошибка запроса /version:", err)
- } else {
-  fmt.Println("Version:", versionResp)
- }
+	fmt.Println("Запрос к /decode")
+	reqBody, _ := json.Marshal(map[string]string{"inputString": "SGVsbG8gd29ybGQ="}) // "Hello world" in base64
+	decodeReq, _ := http.NewRequestWithContext(ctx, "POST", "http://localhost:8080/decode", bytes.NewBuffer(reqBody))
+	decodeReq.Header.Set("Content-Type", "application/json")
+	decodeResp, err := client.Do(decodeReq)
+	handleResponse("POST /decode", decodeResp, err)
 
-  encoded := base64Encoder("Hello, World!")
-  body := map[string]string{"inputString": encoded}
-  decodedResp, err := doPost(ctx, client, baseURL+"/decode", body)
-  if err != nil {
-    fmt.Println("Ошибка запроса /decode:", err)
- } else {
-    fmt.Println("Decoded:", decodedResp)
- }
-
- hardOpResp, err := doGet(ctx, client, baseURL+"/hard-op")
- if err != nil {
-   fmt.Println("Ошибка запроса /hard-op:", err)
- } else {
-   fmt.Println("Hard-op response:", hardOpResp)
- }
+	fmt.Println("Запрос к /hard-op")
+	hardOpReq, _ := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/hard-op", nil)
+	hardOpResp, err := client.Do(hardOpReq)
+	handleResponse("GET /hard-op", hardOpResp, err)
 }
 
-func doGet(ctx context.Context, client *http.Client, url string) (string, error) {
-  req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-  if err != nil {
-    return "", err
- }
-
- resp, err := client.Do(req)
- if err != nil {
-   return "", err
- }
- defer resp.Body.Close()
-
- body, _ := io.ReadAll(resp.Body)
- return string(body), nil
-}
-
-func doPost(ctx context.Context, client *http.Client, url string, payload interface{}) (string, error) {
- data, err := json.Marshal(payload)
- if err != nil {
-  return "", err
- }
-
- req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(data))
- if err != nil {
-  return "", err
- }
- req.Header.Set("Content-Type", "application/json")
-
- resp, err := client.Do(req)
- if err != nil {
-  return "", err
- }
- defer resp.Body.Close()
-
- body, _ := io.ReadAll(resp.Body)
- return string(body), nil
-}
-
-func base64Encoder(input string) string {
- return base64.StdEncoding.EncodeToString([]byte(input))
+func handleResponse(name string, resp *http.Response, err error) {
+	if err != nil {
+		fmt.Printf("%s Ошибка: %v\n", name, err)
+		return
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%s: Статус %d, Ответ: %s\n", name, resp.StatusCode, string(body))
 }
